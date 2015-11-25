250a22c581e5eb3c604fc95acffd5423fdfacc30
Richard Nam <richardhnam@gmail.com>
2015-11-24 22:24:54 -0500

    new script

--- 112415 - cleaning.py
+++ 112415 - cleaning.py
@@ -1,66 +1,58 @@
 import numpy as np
 import pandas as pd
+import glob, warnings
+warnings.filterwarnings('ignore')
 
-
-
-'''
-Reads in the data and return the string of text
-'''
-def ReadData(path):
-    data = []
-    with open(path) as f:
-        for line in f:
-            row  = line.split('\t')
-            if len(row) == 22:
-                data.append(row[17])
-            else:
-                data.append('SKIPPED')
-    return data
-
-
-'''
-imports a list of reddit acroymns (spelled wrong)
-appends a few written in hand
-'''
 def RemoveList(path):
     removeList = []
     with open(path) as f:
         for i in f:
-             removeList.append(i.strip())
+            removeList.append(i.strip())
     # add a couple of heuristic
     removeList = ['/r/', ''] + removeList
     return removeList
 
-
-'''
-removes the acros, drops an sample with hyperlink or
-that has the [deleted] string in it.
-'''
-def CleanStrings(sentences, removeList):
-    sentences2 = []
-    for line in sentences:
-        tempSentences = []
-        row = [k.strip() for k in line.split(' ')]
-        for word in row:
-            if word in removeList:
-                pass
-            else:
-                tempSentences.append(word)
-        if tempSentences and (tempSentences[0] == '[deleted]'):
-            sentences2.append('SKIPPED')
-        elif tempSentences and ('http' in ' '.join(tempSentences)):
-            sentences2.append('SKIPPED')
-        elif tempSentences:
-            sentences2.append(' '.join(tempSentences))
-        else:
-            sentences2.append('SKIPPED')
-    return sentences2    
-
-
+def CleanIt(rr):
+    path ='/Users/richardnam/Google Drive/nyuClasses/SNLP/local project folder/subreddit_files'
+    filenames = glob.glob(path + "/*.csv")
+    outfile = open('data.txt', 'w')
+    for doc in filenames:
+        try:
+            df = pd.read_csv(doc)
+            score = df.score.values
+            label = df.subreddit.values
+            body = df.body.values
+            skip_count = 0
+            print_count = 0
+            for index, string in enumerate(body):
+                try:
+                    if string and ("http" in string):
+                        continue
+                    elif string and ("[deleted]" in string):
+                        continue
+                    elif string:
+                        tempString = []
+                        row = [i.strip() for i in string.split(" ")]
+                        for word in row:
+                            if word not in rr:
+                                tempString.append(word)
+                        string = ' '.join([str(i) for i in tempString]).replace('\n', " ")
+                    else:
+                        continue
+                except:
+                    skip_count += 1
+                if score[index] and label[index] and string :
+                    outfile.write('\t'.join([str(label[index]), str(score[index]), str(string)]) + "\n")
+                    print_count += 1
+            print label[0], "skip:", skip_count, "printed:", print_count
+        except:
+            print "Dropped:", doc
+    outfile.close()
 
 if __name__ == '__main__':
-    sentences = ReadData('sample.txt')
-    rr = RemoveList('til.txt')
-    gg = CleanStrings(sentences, rr)
+    til = '/Users/richardnam/Google Drive/nyuClasses/SNLP/snlp project/til.txt'
+    rr = RemoveList(til)
+    gg = CleanIt(rr)
+
 
 
