2e8cf70d25a43bea2935b6f086c6a9d1e36ebf76
jgutman <jacqueline.gutman@gmail.com>
2015-12-04 01:28:48 -0500

    still fixing tf-idf matrix

--- baseline_word2vec.py
+++ baseline_word2vec.py
@@ -9,6 +9,7 @@ import numpy as np
 
 from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
 from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
 
 from sklearn.preprocessing import LabelEncoder
 import random, sys, time
@@ -37,7 +38,7 @@ def tokenize(textdf):
 # Code below based on Kaggle competition: Bag of Words Meets Bags of Popcorn
 # Credited to Angela Chapman https://github.com/wendykan/DeepLearningMovies/
 
-def makeFeatureVec(words, model, num_features):
+def makeFeatureVec(words, weights = None, model, num_features):
     # Function to average all of the word vectors in a given paragraph
     # Pre-initialize an empty numpy array (for speed)
     featureVec = np.zeros((num_features,) , dtype="float32")
@@ -48,31 +49,41 @@ def makeFeatureVec(words, model, num_features):
     index2word_set = set(model.index2word)
     
     # Loop over each word in the reddit post and, if it is in the model's
-    # vocaublary, add its feature vector to the total
+    # vocabulary, add its feature vector to the total
     for word in words:
         if word in index2word_set: 
             nwords = nwords + 1.
-            featureVec = np.add(featureVec,model[word])
+            if (weights == None):
+            	featureVec = np.add(featureVec,model[word])
+            else:
+            		
 	
     # Divide the result by the number of words to get the average
     featureVec = np.divide(featureVec,nwords)
     return featureVec
 
-def getAvgFeatureVecs(documents, model, num_features):
+def getAvgFeatureVecs(documents, weights = None, model, num_features):
     # Given a collection of documents (each one a list of words), calculate 
     # the average feature vector for each one and return a 2D numpy array 
     
     counter = 0.
+    feature_names = None
+    if (weights != None):
+    	feature_names = weights.get_feature_names() 
     
     # Preallocate a 2D numpy array, for speed
     docFeatureVecs = np.zeros((len(documents),num_features),dtype="float32")
 
-    for post in documents:
+	for post in documents:
        # Print a status message every 1000th review
-       if counter%1000. == 0.:
-           print "Reddit post %d of %d" % (counter, len(documents))
-           
-       docFeatureVecs[counter] = makeFeatureVec(post, model, num_features)
+    	if counter%1000. == 0.:
+        	print "Reddit post %d of %d" % (counter, len(documents))
+        	if (weights == None):
+    			docFeatureVecs[counter] = makeFeatureVec(post, model, num_features)
+    		else:
+    			weightPost = weights[counter]
+    			docFeatureVecs[counter] = makeFeatureVec(post, weightPost, model, 
+    				num_features, feature_names)  		
        counter = counter + 1.
     return docFeatureVecs
 
@@ -98,37 +109,78 @@ def baselineWord2Vec(train, test, trainDataVecs, testDataVecs):
 	
 	print 'CLASSES', le.classes_ 
 
+def docWordList(text, remove_stopwords = False, to_lower = False):
+	# Function to convert a document to a sequence of words,
+    # optionally removing stop words.  Returns a list of words.
+    if to_lower:
+    	words = text.lower().split()
+    else:
+    	words = text.split()
+    if remove_stopwords:
+    	stops = set(stopwords.words("english"))
+    	words = [w for w in words if not w in stops]
+    	
+    return words
+    
 def main():
 	google_drive = os.path.abspath('../../Google Drive/gdrive/')
 	
-	parser = argparse.ArgumentParser(description='Get word2vec model path')
-	parser.add_argument('-w2v', dest = 'w2vpath', help='location of pre-built word2vec model')
-	parser.add_argument('-train', dest = 'trainpath', help='location of pre-split training data')
-	parser.add_argument('-test', dest = 'testpath', help='location of pre-split test data')
-	parser.add_argument('-data', dest = 'datapath', help='location of unsplit data file')
-	parser.add_argument('-split', dest = 'splitdata', help='split data into train and test?',
-		action='store_true')
+	parser = argparse.ArgumentParser(description = 'Get word2vec model path')
+	parser.add_argument('-w2v', dest = 'w2vpath', help = 'location of pre-built word2vec model')
+	parser.add_argument('-train', dest = 'trainpath', help = 'location of pre-split training data')
+	parser.add_argument('-test', dest = 'testpath', help = 'location of pre-split test data')
+	parser.add_argument('-data', dest = 'datapath', help = 'location of unsplit data file')
+	parser.add_argument('-split', dest = 'splitdata', help = 'split data into train and test?',
+		action = 'store_true')
+	parser.add_argument('-weighted', dest = 'weightedw2v', help = 'use tf-idf weighting for words',
+		action = 'store_true')
+	parser.add_argument('-stopwords', dest = 'removeStopWords', help = 'remove English stop words',
+		action = 'store_true')
 	
 	parser.set_defaults(w2vpath = os.path.join(google_drive, 'w2v_output1/w2v_output01.txt'), 
 		trainpath = os.path.join(google_drive, 'data/train.txt'), 
 		testpath = os.path.join(google_drive, 'data/test.txt'),
-		datapath = os.path.join(google_drive, 'data.txt'), splitdata = False)
+		datapath = os.path.join(google_drive, 'data.txt'), 
+		splitdata = False, weightedw2v = False, removeStopWords = False)
 	args = parser.parse_args()
 	
 	model = models.Word2Vec.load(args.w2vpath)
-	if (args.splitdata):
+	if args.splitdata:
 		train, test = write_training(args.datapath, args.trainpath, args.testpath)
 	else:
-		train = pd.read_csv(args.trainpath, sep = '\t', header=None, names = ['label', 'score', 'text'])
-		test = pd.read_csv(args.trainpath, sep = '\t', header=None, names = ['label', 'score', 'text'])
+		train = pd.read_csv(args.trainpath, sep = '\t', header=None, 
+			names = ['label', 'score', 'text'])
+		test = pd.read_csv(args.trainpath, sep = '\t', header=None, 
+			names = ['label', 'score', 'text'])
 	
 	word_vectors = model.syn0
 	vocabulary_size = word_vectors.shape[0]
 	num_features = word_vectors.shape[1]
 	
 	# Should we remove stopwords here or just implement the tf-idf weighting scheme?
-	trainDataVecs = getAvgFeatureVecs(train['text'], model, num_features)
-	testDataVecs = getAvgFeatureVecs(test['text'], model, num_features)
+	train_words = []
+	test_words = []
+	for post in train['text'].astype(str):
+		train_words.append(docWordList(post, remove_stopwords = args.removeStopWords))
+	for post in test['text'].astype(str):
+		test_words.append(docWordList(post, remove_stopwords = args.removeStopWords))
+	
+	if args.weightedw2v:
+		tf = TfidfVectorizer(analyzer='word', vocabulary = model.vocab.keys(),
+			stop_words = ('english' if args.removeStopWords else None))
+		tfidf_matrix_train =  tf.fit_transform(train['text'].astype(str))
+		tfidf_matrix_test =  tf.fit_transform(test['text'].astype(str))
+		
+		trainDataVecs = getAvgFeatureVecs(train_words, weights = tfidf_matrix_train, 
+			model = model, num_features = num_features)
+		testDataVecs = getAvgFeatureVecs(test_words, weights = tfidf_matrix_test, 
+			model = model, num_features = num_features)
+		
+		
+		
+	else: 
+		trainDataVecs = getAvgFeatureVecs(train_words, model = model, num_features = num_features)
+		testDataVecs = getAvgFeatureVecs(test_words, model = model, num_features = num_features)
 	
 	baselineWord2Vec(train, test, trainDataVecs, testDataVecs)
 
