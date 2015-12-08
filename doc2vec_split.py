075ec7223b2466d409bb0efc34d78b4bea7c62d3
jgutman <jacqueline.gutman@gmail.com>
2015-12-08 16:29:10 -0500

    use training only, still no doctors

---
+++ doc2vec_split.py
@@ -0,0 +1,73 @@
+'''
+In this script a  doc2vec model is training.
+First, any rows with missing values are dropped.
+Second, The text for each post is unravel, and written to a file
+so that there is only one sentence per line (~9 million sentences)
+Next, a doc2Vec model is train and return, also the model is saved
+to the current directory.
+'''
+import pandas as pd
+import numpy as np
+from gensim.models import doc2vec
+import sys, time
+
+
+# Parse cleaned data into a vector
+def ParseData(path):
+    '''
+    arguments: path = path to data.txt
+    returns: an array of data
+    '''
+    tempD = []
+    skip_count = 0
+    with open(path) as f:
+        try:
+            for line in f:
+                row = line.split('\t')
+                label = row[0]
+                score = row[1]
+                text = ' '.join(row[2:]).replace("\t", " ").strip()
+                if label != 'nan':
+                    tempD.append([label, score, text])
+        except:
+            skip_count +=1
+    sys.stdout.write('\rerrors catched: %d' % skip_count)
+    return tempD
+
+# Create a file of just sentences for NN, break at '.'
+def Sentences(data, filename='docs'):
+    '''
+    arguement: data = array of data, filename = file name
+    returns: string of the path to sentences
+    '''
+    output = open(filename+'.txt', 'w')
+    for row in data:
+        text = row[2]
+        if str(text).strip():
+            output.write(str(text).strip()+"\n")
+    output.close()
+    sys.stdout.write('\rreturns path to file: ' + filename +'.txt')
+    return filename+'.txt'
+
+# Word2Vec model
+def d2v(sentencepath, length=300, samples=10, alpha_limit=0.0001, epochs=1, output='doc2vec_model.txt'):
+    sentences = doc2vec.TaggedLineDocument(sentencePath)
+    model = doc2vec.Doc2Vec(sentences, size=length, 
+                            min_alpha=alpha_limit, negative=samples,
+                            iter=epochs, min_count=100, workers=4)
+    model.save(output)
+    sys.stdout.write("\rAll done. Model saved to " + output)
+    return model
+
+
+if __name__ == '__main__':
+    script, trainpath = sys.argv
+    print 'start'
+    train = pd.read_csv(trainpath, sep = '\t', header=None, 
+			names = ['label', 'score', 'text'])
+    sentencePath = Sentences(data=train, filename='train_d2v_ready')
+    start_time = time.time()
+    model = d2v(sentencepath=sentencePath, length=300, 
+                samples=10, alpha_limit=0.001, epochs=5, output='d2v_train_only_labels.txt')
+    lapse = time.time() - start_time
+    print lapse / 60 
--- evaluation.py
+++ evaluation.py
@@ -1,15 +1,21 @@
 import pandas as pd
 from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
 
 def readProbability(pathToFile, header = True):
-
+	
 
 def main():
 	google_drive = os.path.abspath('../../Google Drive/gdrive/')
 	parser = argparse.ArgumentParser(description = 'Get predict_proba path')
 	parser.add_argument('-model', dest = 'modelpath', help = 'path of model probability vectors')
-	parser.set_defaults(modelpath = os.path.join(google_drive, 'baseline/predict_proba.csv'))
+	parser.add_argument('-header', dest = 'hasheader', help = 'probability file has header row',
+		action = 'store_true')
+	parser.set_defaults(modelpath = os.path.join(google_drive, 'baseline/predict_proba.csv'),
+		hasheader = False)
 	args = parser.parse_args()
+	
+	prob = readProbability(args.modelpath, args.hasheader)
 
 if __name__ == '__main__':
 	print 'start'
