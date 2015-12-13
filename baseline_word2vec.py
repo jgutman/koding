c932895d836c57a4679c95df1040ff5214d3cb10
jgutman <jacqueline.gutman@gmail.com>
2015-12-13 18:19:58 -0500

    update

--- baseline_word2vec.py
+++ baseline_word2vec.py
@@ -8,13 +8,17 @@ import pandas as pd
 import numpy as np
 
 from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
-from sklearn.linear_model import LogisticRegression
-from sklearn import svm
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 
 from sklearn.preprocessing import LabelEncoder
 import random, sys, time
+
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.cross_validation import train_test_split
 
 def write_training(datapath, trainpath, testpath):
 	#train, test = Split(datapath)
@@ -125,30 +129,81 @@ def logitWord2Vec(train, test, trainDataVecs, testDataVecs, outputPath):
 	sys.stdout.write('CLASSES: %s\n' % le.classes_)
 	sys.stdout.flush() 
 
-def svmWord2Vec(train, test, trainDataVecs, testDataVecs, outputPath):
-	# Extend Richard's baseline() function in baseline.py to use trainDataVecs / testDataVecs
-	# instead of count vectorizer
+def trainValidationSplit(data, dataY, random_seed = 100, strat_size = 20000):
+	groups = data.unique()
+    random_state = RandomState(seed = random_seed)
+    
+    # training and validation
+    subtrain = pd.DataFrame(data = None, columns = ['label', 'score', 'text']) 
+    val = pd.DataFrame(data = None, columns = ['label', 'score', 'text']) 
+    for i in groups:
+    	subtrain_i, val_i = train_test_split(data[data.label == i], 
+    		test_size = strat_size, random_state = random_state)
+    	subtrain = subtrain.append(subtrain_i)
+    	val = val.append(val_i)
+    
+    return subtrain, val, dataY.loc[subtrain.index], dataY.loc[val.index]
 	
+def svmWord2Vec(train, test, trainDataVecs, testDataVecs, outputPath, lamb, zoom, 
+		random_seed = 100, strat_size = 20000):
+		
 	# encode labels
 	le = LabelEncoder()
 	le.fit(train.label)
 	train['y'] = le.transform(train.label)
 	test['y'] = le.transform(test.label)
 	
-	# train model
-	C = 1.0  # SVM regularization parameter
-	model = svm.SVC(kernel='linear', C=C).fit(trainDataVecs, train.y.values)
+	# split training into subtraining and validation
+	subtrain, val, subtrain_Y, val_Y = trainValidationSplit(train, train.y.values,
+		random_seed, strat_size)
+	subtrain_X = trainDataVecs[subtrain.index, :]
+	val_X = trainDataVecs[val.index, :]
 	
-	sys.stdout.write('Test sample score: %0.4f\n' % model.score(testDataVecs, test.y.values))
-	sys.stdout.write('In sample scores: %0.4f\n' % model.score(trainDataVecs, train.y.values))
-	sys.stdout.flush()
-
-	outfile = os.path.join(outputPath, 'word2vec_svm_predict_proba.csv')
-	pd.DataFrame(model.predict_proba(testDataVecs)).to_csv(outfile, 
-		sep = '\t', header = list(le.classes_), index = False)
-	
-	sys.stdout.write('CLASSES: %s\n' % le.classes_)
-	sys.stdout.flush()
+	sys.stdout.write('train_count dims: %s\n' % str(subtrain_X.shape))
+    sys.stdout.write('validation_count dims: %s\n' % str(val_X.shape))
+    sys.stdout.write('test_count dims: %s\n' % str(testDataVecs.shape))
+    sys.stdout.write('validation_bins dims: %s\n' % str(np.bincount(val_Y)))
+    sys.stdout.write('test_bins dims: %s\n' % str(np.bincount(test.y.values)))
+    sys.stdout.flush() 
+    
+    lower = 1e-6
+    upper = 10
+    
+	for level in xrange(zoom):
+        lambda_range = np.linspace(lower, upper, lamb)
+        nested_scores = []
+        for i, v in enumerate(lambda_range):
+            clf = SGDClassifier(alpha=v, loss='hinge', penalty='l2', 
+                                l1_ratio=0, n_iter=5, n_jobs=4, shuffle=True,  
+                                learning_rate='optimal', class_weight="balanced")
+            model = clf.fit(subtrain_X, sub_train_Y)
+            nested_scores.append(model.score(val_X, val_Y))
+            sys.stdout.write('level: %d lambda: %0.4f score: %0.4f\n' % (level, v, model.score(val_X, val_Y))
+            sys.stdout.flush()
+        best = np.argmax(nested_scores)
+        # update the lower and upper bounds
+        if best == 0:
+            lower = lambda_range[best]
+            upper = lambda_range[best+1]
+        elif best == lamb-1:
+            lower = lambda_range[best-1]
+            upper = lambda_range[best]
+        else:
+            lower = lambda_range[best-1]
+            upper = lambda_range[best+1]
+        sys.stdout.write('best: %0.4f score: %0.4f\n'  % (best, nested_scores[best])
+        sys.stdout.flush()
+    clf = SGDClassifier(alpha=lambda_range[best], loss='hinge', penalty='l2', 
+                        l1_ratio=0, n_iter=5, n_jobs=4, shuffle=True,  
+                        learning_rate='optimal', class_weight="balanced")
+    model = clf.fit(subtrain_X, subtrain_Y)
+    df = pd.DataFrame(model.decision_function(testDataVecs), 
+                      columns=[v for i,v in enumerate(le_classes_)])
+	df['y'] = test.y.values
+    df['predict'] = model.predict(testDataVecs)
+    df.to_csv('decision_function_svm_word2vec.csv', sep='\t', index=False)
+    sys.stdout.write('FINAL SCORE %0.4f\n' % model.score(testDataVecs, test.y.values))
+    sys.stdout.flush()
 
 def docWordList(text, remove_stopwords = False, to_lower = False):
 	# Function to convert a document to a sequence of words,
@@ -255,7 +310,7 @@ def main():
 	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 	logitWord2Vec(train, test, trainDataVecs, testDataVecs, outputDirectory)
 	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-	svmWord2Vec(train, test, trainDataVecs, testDataVecs, outputDirectory)
+	svmWord2Vec(train, test, trainDataVecs, testDataVecs, outputDirectory, )
 
 if __name__ == '__main__':
 	sys.stdout.write("start!\n"); sys.stdout.flush()
