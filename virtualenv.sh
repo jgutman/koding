36a558455320847914b9ccae7cf6d4632b065326
jgutman <jacqueline.gutman@gmail.com>
2015-12-13 01:58:25 -0500

    setup virtualenv on hoc

---
+++ virtualenv.sh
@@ -0,0 +1,16 @@
+module load virtualenv/12.1.1
+cd $SCRATCH/reddit_classification
+virtualenv env
+source env/bin/activate
+
+pip install --upgrade pip
+pip wheel --wheel-dir=./wheelhouse pandas==0.16.2	
+pip install --no-index --find-links=./wheelhouse pandas==0.16.2
+
+pip freeze > requirements.txt
+deactivate
+
+module load virtualenv/12.1.1
+pip install --upgrade -r requirements.txt
+
+
