d6221b42e658d2a7aab223c5b2ad5a905c8589c4
jgutman <jacqueline.gutman@gmail.com>
2015-12-15 01:04:25 -0500

    fix

--- evaluation.py
+++ evaluation.py
@@ -59,7 +59,6 @@ def main():
 	parser.set_defaults(datapath = os.path.join(root_directory, 'data/data3.txt'),
 		modelpath = os.path.join(root_directory, 'bayes_baseline/predict_proba_NB_baseline_ngram-1.csv'),
 		hasheader = False, index = False, svmmodel = False, sep = ",", 
-		datapath = os.path.join(root_directory, 'data/data3.txt'),
 		outpath = os.path.join(root_directory, 'bayes_baseline/confusion.png'))
 	args = parser.parse_args()
 	
