743c1c55b1442b088ae68d125da7bf2b09a7069b
jgutman <jacqueline.gutman@gmail.com>
2015-12-16 15:40:27 -0500

    editing d2v method, input done

--- build_tune_d2v.py
+++ build_tune_d2v.py
@@ -79,30 +79,38 @@ def tokenize_text( data, filename, tolowercase = True ):
 	
 	sys.stdout.write("Tokenizing %d documents\n" % data.shape[0]); sys.stdout.flush()
 	for row in data.itertuples():
-		text = str(row[3])
+		if (row[0] % 100000. == 0):
+			sys.stdout.write("Document %d\n" % row[0]); sys.stdout.flush()
+		text = str(row[3]).strip()
 		text = space.join(re.split(pattern, text))
 		if tolowercase:
 			text = text.lower()
-		data.ix['text'][row[0]] = text # change value in dataframe, not on copy of slice
-		output.write(text+'\n')
+		# data.loc[row[0], 'text'] = text # change value in dataframe, not on copy of slice
+		output.write("%s\n" % text)
 	
 	output.close()
 	sys.stdout.write("Documents tokenized to %s\n" % filename); sys.stdout.flush()
-	
-def main():
+
+def modifyDataFromFile( data, filename):
+	newtext = open(filename, 'r')
+	docs = newtext.readlines()
+	data.text = [line.rstrip() for line in docs]
+
+def parseArgs():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("-context", dest = context)
-	parser.add_argument("-dims", dest = dims)
-	parser.add_argument("-root", dest = root_dir)
-	parser.add_argument("-data", dest = data_path)
-	parser.add_argument("-d2v", dest = store_d2v)
-	parser.add_argument("-out", dest = store_out)
-	parser.add_argument("-sentences", dest = tokenized_text)
-	parser.add_argument("-subsets", dest = subset_path)
-	parser.add_argument("-tokenized", dest = pre_tokenized, action = store_true)
-	parser.add_argument("-loadsplit", dest = load_split_data, action = store_true)
+	parser.add_argument("-context", dest = "context", type = int)
+	parser.add_argument("-dims", dest = "dims", type = int)
+	parser.add_argument("-root", dest = "root_dir")
+	parser.add_argument("-data", dest = "data_path")
+	parser.add_argument("-d2v", dest = "store_d2v")
+	parser.add_argument("-out", dest = "store_out")
+	parser.add_argument("-sentences", dest = "tokenized_text")
+	parser.add_argument("-subsets", dest = "subset_path")
+	parser.add_argument("-tokenized", dest = "pre_tokenized", action = "store_true")
+	parser.add_argument("-loadsplit", dest = "load_split_data", action = "store_true")
 	
-	parser.set_defaults(context = "5", dims = "100", pre_tokenized = False,
+	parser.set_defaults(context = 5, dims = 100, 
+		pre_tokenized = False, load_split_data = False
 		root_dir = "/home/cusp/rn1041/snlp/reddit/nn_reddit",
 		data_path = "/data/data3.txt",
 		store_d2v = "/d2vtune/embeddings/",
@@ -110,23 +118,46 @@ def main():
 		tokenized_text = "/d2vtune/tokenized.txt",
 		subset_path = "/d2vtune/data/")
 	args = parser.parse_args()
+	return args
+
+class argdict:
+	def __init__(self, context=5, dims=100):
+		self.context = context
+		self.dims = dims
+		self.root_dir = "/home/cusp/rn1041/snlp/reddit/nn_reddit"
+		self.data_path = "data/data3.txt"
+		self.store_d2v = "d2vtune/embeddings/"
+		self.store_out = "d2vtune/predictions/"
+		self.tokenized_text = "d2vtune/tokenized.txt"
+		self.subset_path = "d2vtune/data/"
+		self.pre_tokenized = True
+		self.load_split_data = True
+		
+	def __init__(self):
+		self.__init__(self, context=5, dims=100)
+
+def main():
+	args = parseArgs()
+	# args = argdict()
 	
 	data_path = os.path.join(os.path.abspath(args.root_dir), args.data_path)
 	store_d2v = os.path.join(os.path.abspath(args.root_dir), args.store_d2v)
 	store_out = os.path.join(os.path.abspath(args.root_dir), args.store_out)
 	tokenized_path = os.path.join(os.path.abspath(args.root_dir), args.tokenized_text)
 	subset_path = os.path.join(os.path.abspath(args.root_dir), args.subset_path)
-	
-	context = int(args.context)
-	dims = int(args.context)
-	
+
 	# parse tokenize split shuffle data
 	sys.stdout.write("Reading, splitting, and shuffling the data\n"); sys.stdout.flush()
 	data = pd.read_csv( data_path, sep = '\t', header = None, 
 			names = ['label', 'score', 'text'] ).dropna()
-			
+	
+	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 	if not args.pre_tokenized:
+		logging.info('tokenizing')
 		tokenize_text( data, tokenized_path )
+		filename = os.path.join(subset_path, 'data.txt')
+		sys.stdout.write("Writing tokenized dataframe to file %s\n" % filename)
+		modifyDataFromFile( data, tokenized_path)
 		data.to_csv(os.path.join(subset_path, 'data.txt'), sep = '\t', header = False, index = False)
 		
 	if args.load_split_data:
@@ -137,17 +168,20 @@ def main():
 			names = ['label', 'score', 'text'], index_col = 0 )
 		test = pd.read_csv( os.path.join( subset_path, 'test.txt'), sep = '\t', header = None,
 			names = ['label', 'score', 'text'], index_col = 0 )
+		sys.stdout.write("loaded data\n train: %s\n val: %s\n test: %s\n" % 
+			(str(train.shape), str(val.shape), str(test.shape)))
 	else:
 		# split data into train, test, and validation stratfied by class
 		# write datasets to file, keep index column
 		train, val, test = trainValidationTest(data, test_size = .15)
+		sys.stdout.write("Writing tokenized dataframe to file %s\n" % filename)
 		train.to_csv( os.path.join(subset_path, 'train.txt'), sep = '\t', header = False, index = True )
 		val.to_csv( os.path.join(subset_path, 'val.txt'), sep = '\t', header = False, index = True )
 		test.to_csv( os.path.join(subset_path, 'test.txt'), sep = '\t', header = False, index = True )
 	
 	# build document embeddings
 	
-	traind2v( train, val, test, context, dims, store_d2v, tokenized = tokenized_path )
+	traind2v( train, val, test, args.context, args.dims, store_d2v)
 	
 	# train SVM on document embeddings
 	
