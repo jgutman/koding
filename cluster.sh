300e5c2ee53e32228f126e4427fb47103681a2e2
jgutman <jacqueline.gutman@gmail.com>
2015-12-19 00:57:26 -0500

    cluster job

---
+++ cluster.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#PBS -l mem=64GB
+#PBS -l walltime=12:00:00
+#PBS -l nodes=1:ppn=8
+#PBS -M jg3862@nyu.edu
+#PBS -m ae
+
+source activate redditenv
+cd /scratch/jg3862/gdrive
+
+python ../koding/cluster_visualize.py > cluster_log.out
