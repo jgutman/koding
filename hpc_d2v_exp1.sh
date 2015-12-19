04509ca5f206247a9914133997cced2742219d88
jgutman <jacqueline.gutman@gmail.com>
2015-12-18 21:10:18 -0500

    update

--- hpc_d2v_exp1.sh
+++ hpc_d2v_exp1.sh
@@ -6,8 +6,7 @@
 #PBS -M jg3862@nyu.edu
 #PBS -m ae
 
-cd $ARCHIVE/anaconda2/bin
-source activate redditenv
-cd $SCRATCH/gdrive
+source /archive/jg3862/anaconda2/bin/activate redditenv
+cd /scratch/jg3862/gdrive
 
 nohup python ../koding/grid_search_d2v.py -context 10 -dims 100 150 200 250 300 400 500 600 -epochs 5 -cores 8 > d2vtune/logs/con10_dimall_epoch5.out
