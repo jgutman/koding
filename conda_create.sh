faea7498e4526bde5bc02252aa29f6d2eeee99fe
jgutman <jacqueline.gutman@gmail.com>
2015-12-18 21:29:08 -0500

    small edits

--- conda_create.sh
+++ conda_create.sh
@@ -1,12 +1,23 @@
-bash $SCRATCH/reddit_classification/Miniconda-1.6.0-Linux-x86_64.sh
-
-# [set installation directory to /archive/jg3862/anaconda2]
+bash /archive/jg3862/Anaconda2-2.4.1-Linux-x86_64.sh
+# [set installation directory to /home/jg3862/anaconda2]
 # [prepend path to .bashrc file]
 # [restart terminal]
 
-conda create --name venv python
+source /home/jg3862/.bashrc
 conda update conda
-source activate venv
-conda list
+conda create --name redditenv python
 
+source activate redditenv
+conda list
 conda install numpy
+conda install scipy
+conda install pandas
+conda install scikit-learn
+conda install matplotlib
+conda install gensim
+conda list
+conda list -e > /scratch/jg3862/reddit_classification/redditenv_conda.txt
+
+python /scratch/jg3862/koding/test_import.py
+
+source deactivate
--- test_import.py
+++ test_import.py
@@ -1,5 +1,7 @@
 import numpy, scipy, pandas, sklearn, matplotlib, gensim
 import os, sys, argparse, time, logging
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 
 modules = [numpy, scipy, pandas, sklearn, matplotlib, gensim]
 for pkg in modules:
